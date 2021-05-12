6169ba556ec07ef57b564bff37168fbaf2d83e6e
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2021-05-12 18:15:05 +0200

    Reactored component using vue composition api

--- src/glitched-writer.vue
+++ src/glitched-writer.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed, watch, onMounted, ref } from 'vue'
 import GlitchedWriter, {
 	WriterDataResponse,
 	presets,
@@ -7,9 +7,12 @@ import GlitchedWriter, {
 	wait,
 	ConstructorOptions,
 } from '../node_modules/glitched-writer'
+import { escapeHtml } from './utils'
 export { GlitchedWriter, presets, glyphs, wait }
 
-export default /*#__PURE__*/ defineComponent({
+type WriterCallback = (string: string, data: WriterDataResponse) => void
+
+export default defineComponent({
 	name: 'GlitchedWriter',
 	props: {
 		text: {
@@ -28,71 +31,94 @@ export default /*#__PURE__*/ defineComponent({
 			type: String,
 			default: 'default',
 		},
-	},
-	data() {
-		return {
-			writer: (null as unknown) as GlitchedWriter,
-		}
-	},
-	computed: {
-		appear(): boolean {
-			return typeof this.$attrs.appear !== 'undefined'
-		},
-		basePreset(): ConstructorOptions {
-			return presets[this.preset as keyof typeof presets] ?? {}
+		silent: {
+			type: Boolean,
+			default: true,
 		},
 	},
-	watch: {
-		text() {
-			this.write()
-		},
-		options() {
-			this.setOptions()
-		},
-		preset() {
-			this.setOptions()
-		},
-		pause(paused: boolean) {
-			paused ? this.writer.pause() : this.writer.play()
-		},
-	},
-	methods: {
-		setOptions() {
-			this.writer.setOptions({
-				...this.basePreset,
-				...this.options,
-			})
-		},
-		setTextContent(text: string): void {
-			if (this.writer.options.html) this.$el.innerHTML = text
-			else this.$el.textContent = text
-		},
-		step(string: string, writerData?: WriterDataResponse) {
-			this.$emit('step', string, writerData)
-		},
-		finish() {
-			if (this.pause) return
-			const { string, writerData } = this.writer
-			this.$emit('finish', string, writerData)
-		},
-		async write() {
-			await this.writer.write(this.text)
-			this.finish()
-		},
-	},
-	mounted() {
-		this.writer = new GlitchedWriter(this.$el, this.options, this.step)
-		this.setTextContent(this.text)
+	setup(props, { emit, attrs }) {
+		const writer = ref((null as unknown) as GlitchedWriter),
+			element = ref(null as null | HTMLElement),
+			preset = computed(
+				// @ts-ignore
+				(): ConstructorOptions => presets[props.preset] ?? {},
+			)
 
-		if (this.appear) {
-			this.setTextContent('')
-			this.write()
-			this.pause && this.writer.pause()
+		/**
+		 * Options will react to preset and options props change
+		 * And force writer to update them internally
+		 */
+		const computedOptions = computed(() => ({
+			...preset.value,
+			...props.options,
+		}))
+		watch(computedOptions, options => writer.value.setOptions(options))
+
+		/**
+		 * Writer state callbacks:
+		 */
+		const onStep: WriterCallback = (string, data) =>
+			emit('step', string, data)
+		const callFinish = () => {
+			if (props.pause) return
+			const { string, writerData } = writer.value
+			emit('finish', string, writerData)
+		}
+
+		async function write() {
+			if (props.pause) return
+			await writer.value.write(props.text)
+			callFinish()
+		}
+		watch(() => props.text, write)
+
+		// Pause and Play
+		watch(
+			() => props.pause,
+			async pause => {
+				if (pause) writer.value.pause()
+				else {
+					await writer.value.play()
+					callFinish()
+				}
+			},
+		)
+
+		/**
+		 * Mounted hook
+		 */
+		onMounted(() => {
+			// Set writer, after DOM is ready
+			writer.value = new GlitchedWriter(
+				element.value,
+				computedOptions.value,
+				onStep,
+			)
+
+			// Write initial text if props.appear is true
+			attrs.appear !== undefined && write()
+		})
+
+		/**
+		 * Initial text will be displayed as html element's content
+		 * but only when appear attribute isn't present
+		 * then it will animate initial text instead
+		 */
+		let initialText: string = ''
+		if (attrs.appear === undefined)
+			initialText = computedOptions.value.html
+				? props.text
+				: escapeHtml(props.text)
+
+		return {
+			writer,
+			element,
+			initialText,
 		}
 	},
 })
 </script>
 
 <template>
-	<span class="glitched-writer"></span>
+	<span class="glitched-writer" ref="element" v-html="initialText"></span>
 </template>
